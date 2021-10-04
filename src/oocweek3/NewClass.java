c13d7c45ca198592dfed1fcac16fd8addbf95904
Amilcar Aponte <apontejaj@gmail.com>
2021-10-04 13:33:04 +0100

    commit

--- src/oocweek3/NewClass.java
+++ src/oocweek3/NewClass.java
@@ -19,7 +19,8 @@ public class NewClass {
         
         System.out.println("this is a test");
 
-        
+        System.out.println("this is a test");
+        System.out.println("this is a test");
         
         System.out.println("This is my first repository");
         System.out.println("This is my first repository");
