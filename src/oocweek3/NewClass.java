17c136cc3cce6dad08e59b4343441e4b36ac0c8b
Amilcar Aponte <apontejaj@gmail.com>
2021-10-04 12:45:26 +0100

    V2
    This is a test. Added a new class.

---
+++ src/oocweek3/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oocweek3;
+
+/**
+ *
+ * @author apont
+ */
+public class NewClass {
+    
+    public NewClass(){
+        System.out.println("new class");
+    }
+    
+}
--- src/oocweek3/OOCWeek3.java
+++ src/oocweek3/OOCWeek3.java
@@ -15,6 +15,9 @@ public class OOCWeek3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("This is my first repository");
+        
     }
     
 }
